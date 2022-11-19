6268b153d17ab8b832f63ca83b9d44dec186f406
NoOnesSoul <98747653+NoOnesSoul@users.noreply.github.com>
2022-11-19 12:41:32 +0100

    kommentek hozzáadása

--- objects.php
+++ objects.php
@@ -1,17 +1,17 @@
 <?php
 class Cardd {
-    public $color;
-    public $value;
-    public $realvalue;
-    public $shown;
+    public $color; //kártya színe (káró, kőr, pikk, treff)
+    public $value;  //kártya megjelenített értéke(2-10, A, J, Q, K)
+    public $realvalue;  //kártya valós, számbeli értéke(pl.: J = 10), betűkkel mégsem számolhatok
+    public $shown;  //kártya helyzete(hátulja van fent, vagy sem)
 
-    function set_card($col, $val) {
+    function set_card($col, $val) { //kártya tulajdonságainak megadása
         $this->color = $col;
         $this->value = $val;
         $this->shown = "card";
-        switch ($this->value) {
+        switch ($this->value) { //valós érték megadása
             case "A":
-                $this->realvalue = array(
+                $this->realvalue = array( //az ásznak két értéke is lehet, úgyhogy...
                     "few" => 1,
                     "much" => 11
                 );
@@ -22,7 +22,7 @@ class Cardd {
                 $this->realvalue = 10;
                 break;
             default:
-                $this->realvalue = $this->value;
+                $this->realvalue = $this->value; //ha egyszerű szám, akkor egyszerű szám
         }
     }
 }
