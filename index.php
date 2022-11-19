98c3b8cb0900f4df5ada606ba190ab57c52a5333
NoOnesSoul <98747653+NoOnesSoul@users.noreply.github.com>
2022-11-19 12:40:01 +0100

    kommentek hozzáadása

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 require_once "objects.php";
 require_once "functions.php";
 
+//gombok kezelése
 if(isset($_POST['hit'])) {
     $_SESSION['state'] = 'hit';
     action($_SESSION['state']);
@@ -12,18 +13,17 @@ if(isset($_POST['hit'])) {
     action('new');
 }
 
+//ha már elindult a játék, ne indítsa újra gombnyomás után
 if(!isset($_SESSION['state'])) {
     $_SESSION['state'] = 'start';
     action($_SESSION['state']);
 }
 
+//ha a jatékos eléri a 21-et, akkor már ne tudjon, csak új játékot kezdeni
 if(getPoint('usr') >= 21) {
     $_SESSION['state'] = "stand";
     action($_SESSION['state']);
 }
-
-
-//print_r($_SESSION);
 ?>
 
 <!DOCTYPE html>
@@ -38,30 +38,30 @@ if(getPoint('usr') >= 21) {
     <body>
         <center>
             <div class="table">
-                <?php if($_SESSION['state'] == "end") {echo action('end');} ?>
+                <?php if($_SESSION['state'] == "end") {echo action('end');} //ha vége a játéknak, jelenítse meg a kapott eredményt ?>
                 <div class="tab">
-                <h2>Dealer(com): <?php if ($_SESSION['state'] == "end") {echo getPoint('com');} else {echo "?";} ?></h2>
+                <h2>Dealer(com): <?php if ($_SESSION['state'] == "end") {echo getPoint('com');} else {echo "?";} //ha vége a játéknak, jelenítse meg az osztó lapjainak értékét ?></h2>
                 <?php for($i = 0; $i <= sizeof($_SESSION['comCards'])-1; $i++) {
-                    include "comCard.php";
+                    include "comCard.php"; //minden kártya egy külön fájl
                 } ?> 
             </div>
             <br />
             <div class="tab">
-                <h2>You: <?php echo getPoint('usr'); ?></h2>
+                <h2>You: <?php echo getPoint('usr'); //írja ki a játékos lapjainak értékét ?></h2>
                 <?php for($i = 0; $i <= sizeof($_SESSION['usrCards'])-1; $i++) {
-                    include "usrCard.php";
+                    include "usrCard.php"; //minden kártya egy külön fájl
                 } ?>
             </div>
             <br />
             <form method="POST">
-                <?php if(getPoint('usr') < 21 && !isset($_SESSION['winner'])) { ?>
-                    <input type="submit" name="hit" value="Hit">
-                    <input type="submit" name="stand" value="Stand">
+                <?php if(getPoint('usr') < 21 && !isset($_SESSION['winner'])) { //ha nincs vége a játéknak, jelenítse meg a "játszó gombokat"?>
+                    <input type="submit" name="hit" value="Hit"> <!-- új lap kérés -->
+                    <input type="submit" name="stand" value="Stand"> <!-- nem kér több lapot -->
                 <?php } ?>
-                    <input type="submit" name="new" value="Start new game">
+                    <input type="submit" name="new" value="Start new game"> <!-- Összes kártya eldobása és játék újrakezdése -->
                 </form>
                 <br />
-                <?php include "rules.html" ?>
+                <?php include "rules.html" ?> <!-- A blackjack játékról információk(egyenlőre hiányos, csak erre a verzióra érvényes infók vannak benne) -->
             </div>
         </center>
     </body>
